65ad9d2fa940d423384e5b7b5a2c6d969e4a7d5f
Sanket Bhadange <sanketnb2404@gmail.com>
2024-04-21 23:03:03 +0530

    imdb part 2 trending movie added

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,33 +1,58 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
+import axios from 'axios'
 
 const Movies = () => {
-  return (
-    <div>
-    <div className='text-2xl font-bold text-center m-8'>Trending Movies</div>
 
-    <div className='flex'>
+    const [moviesData, setMoviesData] = useState([])
 
-          <div className='w-[160px] h-[30vh] bg-center bg-cover m-4 md:h-[40vh] md:w-[180px] relative rounded-xl hover:scale-110 duration-300 flex items-end'
-            style={{
-                backgroundImage: `url(https://images.pexels.com/photos/2108845/pexels-photo-2108845.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`
-              }}
-          >
-          </div>
+    const getTrendingMoviesData = () => {
+        axios
+        .get(`https://api.themoviedb.org/3/trending/movie/day?api_key=4359166d8b1186f0cef3de2ab79e3ac5`)
+        .then(response => {
+          setMoviesData(response.data.results)
+        })
+      }
 
-          <div className='w-[160px] h-[30vh] bg-center bg-cover m-4 md:h-[40vh] md:w-[180px] relative rounded-xl hover:scale-110 duration-300 flex items-end'
-            style={{
-                backgroundImage: `url(https://images.pexels.com/photos/2108845/pexels-photo-2108845.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`
-              }}
-          >
-          </div>
+      useEffect(()=>{
+        getTrendingMoviesData()
+      },[])
 
-          
+    return (
+      <div>
+      <div className='text-2xl font-bold text-center m-8'>Trending Movies</div>
+  
+      <div className='flex flex-wrap'>
+  
+            {
+               moviesData.map(movie=>{
 
-     </div>
-    
-  </div>
+                       return  <div key={movie.id}
+                                     className='w-[160px] h-[30vh] bg-center bg-cover m-4 md:h-[40vh] md:w-[180px] relative rounded-xl hover:scale-110 duration-300 flex items-end'
+                                    style={{
+                                    backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie.poster_path})`
+                                    }}
+                    
+                                >
 
-  )
-}
+                                <div className='text-white font-bold text-center w-full bg-gray-900 bg-opacity-60'>
+                                                {movie.original_title}
+                                </div>
 
-export default Movies
+
+
+                         </div>
+
+                })
+            }    
+            
+  
+  
+  
+       </div>
+  
+    </div>
+  
+    )
+  }
+  
+  export default Movies
